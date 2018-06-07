5c490e9acf839e3858ad19143b62fc69222188c5
Sergey <serega22111991@gmail.com>
2018-06-08 01:18:57 +0300

    start

---
+++ korolumba.xyz/www/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+
+<meta charset="utf-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+<?php
+   mail('slm.widget.sergey@gmail.com','test','hello');
+?>
+
+
+	
+</body>
+</html>					
